d989bb2c0135ae911314568f94205df026111aa2
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-04 14:36:53 -0600

    Partially implemented Start functionality

--- Hangman/Game.cs
+++ Hangman/Game.cs
@@ -3,25 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Hangman
 {
     internal class Game
     {
-        public GuessWord CurrentWord { get; set; }
-        public List<string> Players { get; set; }
-        public int currentPlayerIndex { get; set; }
+        public GuessWord CurrentWord { get; set; }      // stores the current guessing word in game sequence
+        public List<string> Players { get; set; }       // stores a list of names of players
+        public int currentPlayerIndex { get; set; }     // stores the current index of the currently guessing player
 
         public Game(string word, List<string> players)
         {
             CurrentWord = new GuessWord(word);
-            Players.AddRange(players);
+            Players = new List<string>(players);
             currentPlayerIndex = 0;
         }
 
         public void Start()
         {
+            bool gameEnded = false;
 
+            /* Add code here to display the current state of game */
+
+            while (!gameEnded)
+            {
+                string currentPlayer = Players[currentPlayerIndex];
+
+                /* Add code here to tell player to pick a letter to guess */
+
+                char guess = ' '; /* Pass guessed letter to here */
+
+                List<int> guessIndecies = CurrentWord.GetIndecies(guess);
+                
+                if (guessIndecies.Count > 0)
+                {
+                    CurrentWord.UpdateProgress(guess, guessIndecies);
+                }
+                else
+                {
+                    CurrentWord.UpdateInvalidCount();
+                }
+
+                /* Add code here to display the current state of the guessed word
+                 * current state can be found through CurrentWord.Progress */
+
+                if (CurrentWord.CheckInvalidCount())
+                {
+                    gameEnded = true;
+                    /* Add code here to handle losing player
+                     * you can get the player name through currentPlayer */
+                    break;
+                }
+
+                if (CurrentWord.CheckProgress())
+                {
+                    gameEnded = true;
+                    /* Add code here to handle winning player
+                     * you can get the player name through currentPlayer */
+                    break;
+                }
+
+                currentPlayerIndex = currentPlayerIndex == Players.Count - 1 ? 0 : currentPlayerIndex + 1;
+            }
         }
     }
 }
